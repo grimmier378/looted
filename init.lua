61444d51b2f2d421658fd41251eb72717b29bf48
grimmier378 <124466615+grimmier378@users.noreply.github.com>
2024-03-08 04:22:17 -0700

    fix: style var
    * Missed a couple PopStyleVar() causing bleeding onto other lua.

--- init.lua
+++ init.lua
@@ -45,9 +45,9 @@ function guiLoot.GUI()
 	imgui.PushStyleVar(ImGuiStyleVar.WindowPadding, ImVec2(1, 0));
 
 	guiLoot.openGUI, guiLoot.shouldDrawGUI = ImGui.Begin('Looted Items##'..mq.TLO.Me.DisplayName(), guiLoot.openGUI, flags)
-	imgui.PopStyleVar()
 	if not guiLoot.shouldDrawGUI then
 		imgui.End()
+		imgui.PopStyleVar()
 		return
 	end
 
@@ -96,7 +96,7 @@ function guiLoot.GUI()
 	contentSizeY = contentSizeY - footerHeight
 
 	guiLoot.console:Render(ImVec2(contentSizeX, contentSizeY))
-	imgui.PopStyleVar()
+	imgui.PopStyleVar(2)
 
 	ImGui.End()
 end
